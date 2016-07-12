0af1c8e5683bf6c6f76bd580ff27463d5eedcf3e
Christopher Lopez <chrisa_lopez@yahoo.com>
2016-07-12 15:35:10 -0700

    Completed step 2

--- tenant.rb
+++ tenant.rb
@@ -2,13 +2,21 @@ class Tenant
 
   attr_accessor :nickname, :occupation
 
-  def initialized(f_name, l_name, born_on, gender)
+  def initialize(f_name, l_name, born_on, gender)
     @f_name = f_name
     @l_name = l_name
     @born_on = born_on
     @gender = gender
   end
 
+  def full_name
+    "#{@f_name} #{" ' " + nickname + " ' " if nickname} #{@l_name}"
+  end
 end
 
-p1 = Tenant.new "Calvin", "Clifford", "male", "December 2"
+p1 = Tenant.new "Calvin", "Clifford", "male", "December 21, 1929"
+p1.nickname ="Bud"
+p1.full_name
+
+
+puts p1
